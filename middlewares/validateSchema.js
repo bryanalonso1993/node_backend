1f29c92424fb12a67a04b0d1c2b2f02e12075739
bryan alonso <bryalmeyda.7@gmail.com>
2021-08-29 15:57:54 -0500

    Se anadio validacion de array devices

--- middlewares/validateSchema.js
+++ middlewares/validateSchema.js
@@ -9,8 +9,24 @@ exports.validateSchemaToken = (req=request, res=response, next) => {
     })
     const { error } = schema.validate({ token });
     if (error) {
-        logger.log({ level:'error', message: `Error validate SchemaToken ${ error.details[0].message }` })
-        throw res.status(500).json({ error: error })
+        logger.log({ level:'error', message: `Error validate SchemaToken ${ error.details[0].message }` });
+        throw res.status(500).json({ error: error });
+    }
+    next();
+}
+
+exports.validateSchemaObjectDevices = (req=request, res=response, next) => {
+    const arrayDevices = req.body;
+    const schema = Joi.object().keys({
+        deviceName: Joi.string().required(),
+        ipAddress: Joi.string().required(),
+        organization: Joi.string().required()
+    });
+    const schemaArray = Joi.array().items(schema);
+    const { error } = schemaArray.validate(arrayDevices);
+    if (error) {
+        logger.log({ level:'error', message: `Error validate SchemaDevices ${ error.details[0].message }` });
+        throw res.status(500).json({ error: error });
     }
     next();
 }
