8c5f9246d94f0b747e264f45d41b49f21c5c3f55
bryan alonso <bryalmeyda.7@gmail.com>
2021-08-29 15:58:27 -0500

    anadio logs de auth

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,7 @@
 const { request, response } = require('express');
+const logger = require('../config/logger');
 const jwt = require('jsonwebtoken');
+
 /**
  * Authentication
  */
@@ -12,21 +14,24 @@ module.exports = (req=request, res=response) => {
      * Basic Authentication 
      */
     if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
-        throw res.status(401).json({ error:'Missing Authorization Header' })
+        logger.log({ level:'error', message: 'Missing Authorization Header'});
+        throw res.status(401).json({ error:'Missing Authorization Header' });
     }
     /**
      * https://jasonwatmore.com/post/2018/09/24/nodejs-basic-authentication-tutorial-with-example-api
      */
     const base64Credentials =  req.headers.authorization.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
-    const [username, password] = credentials.split(':');    
+    const [username, password] = credentials.split(':');
     if ( username === process.env.USERAPI && password === process.env.PASSAPI)
     {
         const token = jwt.sign({
             data: username
         }, process.env.SEED, { expiresIn: '1h'});
+        logger.log({ level:'info', message:`Success Authorization token generate :${token}`});
         res.status(200).json({ token: token });
     }else{
+        logger.log({ level: 'error', message: 'Error en la authentication, no se genero token'});
         throw res.status(404).json({ error: 'Error de authenticacion'});
     }
 }
